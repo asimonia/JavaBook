4bf50fd34dcc711cee73d7b1d5590b294e040849
Alex Simonian <alex.simonian@gmail.com>
2017-05-27 14:56:36 -0500

    3 and 4

---
+++ Exercise4/TVActorDriver.java
@@ -0,0 +1,23 @@
+public class TVActorDriver {
+
+	public static void main(String[] args) {
+		TVActor a = new TVActor();
+		a.name = "Thomas Middleditch";
+		a.role = "Richard Hendricks";
+		a.age = 20;
+
+		TVActor b = new TVActor();
+		b.name = "Martin Starr";
+		b.role = "Bertram Gilfoyle";
+		b.age = 25;
+
+		TVActor c = new TVActor();
+		c.name = "Kumail Nanjiani";
+		c.role = "Dinesh Chugtai";
+		c.age = 30;
+
+		System.out.println( a.name + " played " + a.role + " and is " + a.age);
+		System.out.println( b.name + " played " + b.role + " and is " + b.age);
+		System.out.println( c.name + " played " + c.role + " and is " + c.age);
+	}
+}
